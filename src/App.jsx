ab67f17d2569cc103c46de29c25536a57a6e8184
NickN1994 <nickneggers@hotmail.com>
2023-12-12 22:37:27 +0100

    nav bar af

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
-
+import {Routes, Route} from 'react-router-dom'
 import './App.css'
+import Navigation from "./components/Navigation.jsx";
 
 function App() {
 
 
   return (
     <>
-
+      <Navigation/>
+        <body>
+      <Routes>
+        {/*<Route path="/" element={<Home/>}/>*/}
+        {/*Dan de about me, dan over werkervaring. Begon als zorgboer, daarna kindercoaching en coaching, geven van opleiding*/}
+        {/*  foto slider maken waarbij foto's laat zien van mij voor een hele zaal en van mijn carriere*/}
+        {/*  dan vertellen over waarom de switch maken naar iets anders */}
+      </Routes>
+        </body>
     </>
   )
 }
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,22 @@
+import './Navigation.css'
+
+
+function Navigation() {
+    return (
+        <header className='outer-container'>
+            <nav>
+                <div>
+                    <h2>Nick Neggers</h2>
+                </div>
+                <ul>
+                    <li>Home</li>
+                    <li>About me</li>
+                    <li>Ervaring</li>
+                    <li>Portfolio</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Navigation;
