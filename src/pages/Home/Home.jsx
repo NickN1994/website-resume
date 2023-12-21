41a950ea742185994584d3104eff2dc5e534fffe
NickN1994 <nickneggers@hotmail.com>
2023-12-21 22:22:25 +0100

    aan aobut me gewerkt

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -26,30 +26,39 @@ function Home() {
 
             <div className='outer-container'>
                 <div className='inner-container'>
-                    <section className='column'>
+                    <section className='column-about-me'>
                         <div>
                             <div className='photoBoxEvent'>
-                            <img src={fotoEvent} alt="foto event" className='photoEvent'/>
+                                <img src={fotoEvent} alt="foto event" className='photoEvent'/>
                             </div>
                         </div>
-                        <div>
+                        <div className='colum-about-me-text'>
                             <h2>About me</h2>
-                            <p>Na 10 jaar zorg is het tijd voor Full Stack Developer. (Voorkeur Frontend)
+                            <p>Na 10 jaar zorg is het tijd voor Full Stack Developer.</p>
+                            <p>(Voorkeur Frontend)</p>
 
-                                Bijna 10 jaar werk ik momenteel in de zorg. Samen met mijn partner hadden we jarenlang een zorgboerderij.
-                                Waarbij ik groepsbegeleider was op de dagbesteding, dagopvang en logeeropvang voor kinderen en jongeren met autisme en/of ADHD. Momenteel bieden we nog voor één dag in de week dagbesteding aan.
+                            <p>Bijna 10 jaar werk ik in de zorg. Samen met mijn partner hadden we jarenlang
+                                een zorgboerderij.
+                                Waarbij ik groepsbegeleider voor kinderen en jongeren met autisme en/of ADHD</p>
 
-                                Waarna ik vervolgens in de coachingwereld terecht kwam. Jarenlang heb ik volwassenen en kinderen gecoacht.
-                                Samen met mijn partner ook een succesvolle coachingopleiding opgezet en samen gegeven.
+                            <p> In 2017 maakte ik een begin in de coachingwereld door als Kindercoach aan het werk te gaan.
+                                Waarna ik ook al snel volwassenen ging coachen en begeleiden.
+                                Dat was voor mijn partner en mij een reden op een coachingopleiding op te zetten.
+                                Die we 5 jaar lang samen succesvol hebben gegeven
 
-                                Maar nu na al die jaren had ik in de zomervakantie besloten om wat anders te gaan doen.
+                                Ook sprak ik bij meerdere events op een podium voor een volle zaal.</p>
+
+                            <p> Maar nu na al die jaren had ik in de zomervakantie besloten om wat anders te gaan doen.
                                 Altijd al heb ik interesse gehad in tech, games en pc.
-                                Al vaker had ik gekeken naar een IT opleiding, maar nooit iets mee gedaan. Maar nu begon het toch te kriebelen.
+                                Al vaker had ik gekeken naar een IT opleiding, maar nooit iets mee gedaan. Maar nu begon
+                                het toch echt te kriebelen.</p>
 
-                                Uiteindelijk toch besloten om de Bootcamp Full Stack Developer te volgen.
-                                Graag zou ik me in deze branch ook verder willen ontwikkelen en een carrière pad bewandelen.
+                            <p> Uiteindelijk toch besloten om de Bootcamp Full Stack Developer te volgen.
+                                Graag zou ik me in deze branch ook verder willen ontwikkelen en een carrière pad
+                                bewandelen.</p>
 
-                                Mijn persoonlijke voorkeur ligt bij de Frontend. Graag zou ik me hier eerst verder op ontwikkelen.</p>
+                            <p> Mijn persoonlijke voorkeur ligt bij de Frontend. Graag zou ik me hier eerst verder op
+                                ontwikkelen.</p>
                         </div>
 
 
